bfe0f5421f371e731b8d261fcfa06164552d18b9
ritsu <ritsuspy@gmail.com>
2025-06-17 13:59:26 +0800

    clean legacy

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="HRI_mllm",
+    version="0.1.0",
+    packages=find_packages(),  # 自动发现所有包
+    install_requires=[
+        # 你的依赖项
+    ],
+)
